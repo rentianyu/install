c24424b5fb420c2ebcbed2a5bbde7d91ee7d29ff
Your Name <you@example.com>
2024-01-13 21:09:18 +0800

    e

--- shizuku/install.sh
+++ shizuku/install.sh
@@ -2,20 +2,20 @@
 # Termux 安装 Shizuku
 # 使用方法：sh <(curl -s https://cdn.jsdelivr.net/gh/rentianyu/install@main/shizuku/install.sh)
 # 作者：[小贝塔](https://github.com/rentianyu)
-# 最后更新时间：2024.01.12
+# 最后更新时间：2024.01.13
 
 # Termux 安装函数
 install() {
     # 下载 dex
-    curl -s https://cdn.jsdelivr.net/gh/rentianyu/install@main/shizuku/rish_shizuku.dex >$PREFIX/bin/rish_shizuku.dex
+    curl -s https://cdn.jsdelivr.net/gh/rentianyu/install@main/shizuku/rish_shizuku.dex >$DIR/rish_shizuku.dex
     # 下载 rish
-    curl -s https://cdn.jsdelivr.net/gh/rentianyu/install@main/shizuku/rish >$PREFIX/bin/rish
+    curl -s https://cdn.jsdelivr.net/gh/rentianyu/install@main/shizuku/rish >$DIR/rish
     # 修改rish里的包名
-    sed -i "s/PKG/com.termux/" $PREFIX/bin/rish
+    sed -i "s/PKG/com.termux/" $DIR/rish
     # 赋予执行权限
-    chmod +x $PREFIX/bin/rish
+    chmod +x $DIR/rish
     # 判断安装成功
-    which rish && echo "Shizuku 安装成功!"
+    which rish && echo "Shizuku 安装成功!" || echo "安装失败。退出！"
 }
 
 # 使用说明
@@ -26,16 +26,36 @@ usage() {
     echo "欢迎加入 小贝塔教程资源 QQ群：773276432"
 }
 
+# 判断当前软件
+cd ~
+if pwd | grep com.termux; then
+    DIR=$PREFIX/bin
+    PKG=com.termux
+
+elif pwd | grep bin.mt.plus; then
+    mkdir -p bin
+    DIR=$HOME/bin
+    PKG=$(pwd | cut -d '/' -f 5)
+    [ -f .bashrc ] && {
+        grep "$(pwd)/bin" .bashrc || echo "export PATH=$PATH:$(pwd)/bin" >>.bashrc
+    } ||
+        echo "export PATH=$PATH:$(pwd)/bin" >>.bashrc
+else
+    echo "当前不是Termux、MT管理器环境，退出!"
+    exit 1
+fi
+
+
 # 安装 Shizuku
-if [ -f "$PREFIX/bin/rish" ]; then
+if [ -f "$DIR/rish" -a -f "$DIR/rish_shizuku.dex" ]; then
     echo "Shizuku已安装,是否重新安装？(y/n): "
     read input
     if [ "$input" = "y" ]; then
         # 强删 rish dex
-        rm -rf $PREFIX/bin/rish
-        rm -rf $PREFIX/bin/rish_shizuku.dex
-        install && usage || echo "安装失败。退出！"
+        rm -rf $DIR/rish
+        rm -rf $DIR/rish_shizuku.dex
+        install && usage
     fi
 else
-    install && usage || echo "安装失败。退出！"
+    install && usage
 fi
