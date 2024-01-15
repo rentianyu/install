a52407b7636ea3fd32eea561a983154a9e4d3d40
Your Name <you@example.com>
2024-01-15 18:32:15 +0800

    去MT，shortcut

--- intent/shortcut.sh
+++ intent/shortcut.sh
@@ -1,99 +1,86 @@
-#!/system/bin/sh
-
-# 获取root权限或shizuku权限 POWER
-power() {
-    su -c "date" && POWER=su && return
-    which rish && POWER=rish && return
-    # shellcheck disable=SC2034
-    # shellcheck disable=SC2088
-    [ -f "~/rish" ] && [ -f "~/rish_shizuku.dex" ] && POWER="sh ~/rish" && return
-    echo "无 su 命令 或 Shizuku 环境"
-    echo "退出!"
-    exit 1
+#!/usr/bin/bash
+brief_introduction() {
+    echo '简介：
+# 抓取桌面shortcut的shell命令
+# 使用方法：bash -c "$(curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/intent/shortcut.sh)""
+# 作者：[小贝塔](https://github.com/rentianyu)
+# 最后更新时间：2024.01.15
+# QQ群：773276432
+生成 Shortcut 的 Shell 命令的文件在：
+$OUT
+$OUT
+$OUT
+请勿执行该文件！
+请勿执行该文件！
+请勿执行该文件！'
 }
 
-# 获取当前软件包名 PKG
-pkg() {
-    cd ~ || exit
-    pwd | grep com.termux && PKG=com.termux
-    pwd | grep bin.mt.plus && PKG=$(pwd | cut -d '/' -f 5)
-    [ -z "$PKG" ] && echo "当前不是Termux或MT管理器环境，退出!" && exit 1
-}
+# 一些变量
+XBT="/storage/emulated/0/A-小贝塔"
+IN="$XBT/shortcuts.txt"
+OUT="$XBT/shortcuts.sh"
 
 # 装shizuku
 install_shizuku() {
-    echo "无 su 命令 或 Shizuku 环境"
+    echo "无 Shizuku 环境"
     echo "是否安装 Shizuku 环境？(y/n)："
     read -r input
     if [ "$input" = "y" ]; then
-        sh <(curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/install.sh)
+        sh -c "$(curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/install.sh)"
     else
         echo "退出!"
         exit 1
     fi
 }
-
-# MT装sed
-install_sed() {
-    sh ~/rish -c ""
-}
-
-
-
-XBT=/storage/emulated/0/XBT
-SHORTCU=$XBT/shortcuts.sh
-
-# 判断是否有 sed 命令
-
-
-
-# 判断是否有 su 命令 或 Shizuku 环境
-if su -c "date"; then
-    C=su
-elif which rish; then
-    C=rish
-elif [ -f "$DIR/rish" ] && [ -f "$DIR/rish_shizuku.dex" ]; then
-    C="sh ~/rish"
-    # sed=
-else
-    echo "无 su 命令 或 Shizuku 环境"
-    echo "是否安装 Shizuku 环境？(y/n)："
-    read -r input
-    if [ "$input" = "y" ]; then
-        sh <(curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/install.sh)
-        package
-    else
-        echo "无 su 命令 或 Shizuku 环境"
-        echo "退出!"
-        exit 1
-    fi
-fi
 
 # 生成 Shortcut 的 Shell 命令
-alias sed="sh ~/rish -c "
-$POWER -c "dumpsys shortcut" |                                                       # 获取shortcut数据
-    grep -e "Package:.*UID" -e longLabel -e intents -e extras |                 # 截取包名、卡片名称、主启动参数、附加启动参数
-    sed 's/.*Package:/\n# 软件包名:/g' |                                            # 包名
-    sed "s/.*longLabel=/# /g;s/, resId.*//g" |                                  # 卡片名称
-    sed "s/.*Intent { /am start --user 0 \'intent:#Intent/g; s/ }\/null]//g" |  # 启动头
-    sed 's/act=/;action=/g' |                                                   # intent 操作
-    sed 's/ dat=/;/g' |                                                         # intent 数据 URI
-    sed 's/ cmp=/;component=/g' |                                               # 带有软件包名称前缀的组件名称
-    sed 's/ flg=/;launchFlags=/g' |                                             # 标记
-    sed 's/ cat=\[/category=/g' |                                               # intent 类别
-    sed 's/\]//g' |                                                             # 删除]
-    sed 's/ pkg=/;package=/g' |                                                 # 包名
-    sed 's/}$//g' |                                                             # 删除结尾}，对应启动头删除{
-    sed "s/.*'.*/\0;end'/g" |                                                   # 结尾标记
-    sed 's/ }\/PersistableBundle\[{/;S./g' |                                    # 参数第一个变 .S  # -e
-    sed 's/, /;S./g' |                                                          # 剩余参数变 .S 字符串
-    sed 's/S\.\(\w\+\=[0-9]\{1,3\};\)/i.\1/g' |                                 # .i 整数型数据  # --ei
-    sed 's/S\.\(\w\+\=true;\)/B.\1/g' |                                         # .B 布尔值数据  # --ez
-    sed 's/S\.\(\w\+\=false;\)/B.\1/g' |                                        # .B 布尔值数据  # --ez
-    sed '/extras=null/d' |                                                      # intent 数据 URI
-    sed "/ \+extras/s/;.*/'/g;/ \+extras/s/.*{\w\+\=/am start -d '/g" >$SHORTCU # intent 数据 URI
+main() {
+    grep -e "Package:.*UID" -e longLabel -e intents -e extras "$1" |               # 截取包名、卡片名称、主启动参数、附加启动参数
+        sed 's/.*Package:/\n# 软件包名:/g' |                                           # 包名
+        sed "s/.*longLabel=/# /g;s/, resId.*//g" |                                 # 卡片名称
+        sed "s/.*Intent { /am start --user 0 \'intent:#Intent/g; s/ }\/null]//g" | # 启动头
+        sed 's/act=/;action=/g' |                                                  # intent 操作  # -a
+        sed 's/ dat=/;/g' |                                                        # intent 数据 URI  # -d
+        sed 's/ cmp=/;component=/g' |                                              # 带有软件包名称前缀的组件名称  # -n
+        sed 's/ flg=/;launchFlags=/g' |                                            # 标记 # -f
+        sed 's/ cat=\[/category=/g' |                                              # intent 类别  # -c
+        sed 's/\]//g' |                                                            # 删除]
+        sed 's/ pkg=/;package=/g' |                                                # 包名
+        sed 's/}$//g' |                                                            # 删除结尾}，对应启动头删除{
+        sed "s/.*'.*/\0;end'/g" |                                                  # 结尾标记
+        sed 's/ }\/PersistableBundle\[{/;S./g' |                                   # 参数第一个变 .S  # -e
+        sed 's/, /;S./g' |                                                         # 剩余参数变 .S 字符串
+        sed 's/S\.\(\w\+\=[0-9]\{1,3\};\)/i.\1/g' |                                # .i 整数型数据  # --ei
+        sed 's/S\.\(\w\+\=true;\)/B.\1/g' |                                        # .B 布尔值数据  # --ez
+        sed 's/S\.\(\w\+\=false;\)/B.\1/g' |                                       # .B 布尔值数据  # --ez
+        sed '/extras=null/d' |                                                     # intent 数据 URI
+        sed "/ \+extras/s/;.*/'/g;/ \+extras/s/.*{\w\+\=/am start -d '/g" >$OUT    # intent 数据 URI
+        [ -s "$1" ] && echo "处理成功" || echo "处理失败！处理失败！处理失败！"
+}
 
-echo "生成 Shortcut 的 Shell 命令的文件在："
-echo "$SHORTCU"
-echo "请勿自行该文件！"
-am start -a android.intent.action.VIEW -d file://$SHORTCU
+# 判断是否是termux
+cd ~ || exit 1
+pwd | grep termux || echo -en "当前非 termux 软件！\n请用root权限运行或把要处理的shortcuts.txt放在同目录下"
+cd - || exit 1
+
+# 打印简介
+brief_introduction
+
+# 判断各情况
+# root
+if su -c "当前是root权限！"; then
+    su -c "dumpsys shortcut" >"$XBT/shortcuts.txt"
+    main "$XBT/shortcuts.txt"
+# 小贝塔文件夹的 shortcuts.txt
+elif [ -f $IN ]; then
+    main "$IN"
+# 当前目录的 shortcuts.txt
+elif [ -f "shortcuts.txt" ]; then
+    main "$IN"
+# 无root、shizuku 询问安装shizuku
+else
+    echo "无 root权限或要处理的Shortcut ！"
+    install_shizuku
+    echo "退出!"
+    exit 1
+fi
--- shizuku/install.sh
+++ shizuku/install.sh
@@ -1,52 +1,44 @@
-#!/bin/sh
-# Termux、MT管理器安装 Shizuku
-# 使用方法：sh <(curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/install.sh)
+#!/usr/bin/bash
+# Termux 安装 Shizuku
+# 使用方法：bash -c "$(curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/install.sh)""
 # 作者：[小贝塔](https://github.com/rentianyu)
 # 最后更新时间：2024.01.13
+# QQ群：773276432
 
 # Termux 安装函数
 install() {
     # 下载 dex
-    curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/rish_shizuku.dex >$DIR/rish_shizuku.dex
+    curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/rish_shizuku.dex >"$DIR/rish_shizuku.dex"
     # 下载 rish
-    curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/rish >$DIR/rish
+    curl -s https://github.moeyy.xyz/https://raw.githubusercontent.com/rentianyu/install/main/shizuku/rish >"$DIR/rish"
     # 修改rish里的包名
-    sed -i "s/PKG/$PKG/" $DIR/rish
+    sed -i "s/PKG/$PKG/" "$DIR/rish"
     # 赋予执行权限
-    chmod +x $DIR/rish
+    chmod +x "$DIR/rish"
     # 判断安装成功
-    [ -f "$DIR/rish" -a -f "$DIR/rish_shizuku.dex" ] && echo -e "\n\n\nShizuku 安装成功!" || echo "安装失败。退出！"
+    [ -f "$DIR/rish" ] && [ -f "$DIR/rish_shizuku.dex" ] && printf "\n\n\nShizuku 安装成功!" || echo "安装失败。退出！"
 }
 
 # 使用说明
 usage() {
-    echo $PKG | grep -q bin.mt.plus && MT='sh ~/'
-    echo -e "\nrish - 使用方法："
-    echo -e "${MT}rish                # 进入交互式终端"
-    echo -e "${MT}rish -c \"command\"     # 可执行单独命令"
-    echo "欢迎加入 小贝塔教程资源 QQ群：773276432"
+    echo "$PKG" | grep -q bin.mt.plus && MT='sh ~/'
+    printf "\nrish - 使用方法："
+    echo "${MT}rish                # 进入交互式终端"
+    echo "${MT}rish -c \"command\"     # 可执行单独命令"
+    printf "欢迎加入 小贝塔教程资源 QQ群：773276432"
 }
 
 # 判断当前软件
 cd ~ || exit
-if pwd | grep com.termux; then
-    DIR=$PREFIX/bin
-    PKG=com.termux
-elif pwd | grep bin.mt.plus; then
-    DIR=$HOME
-    PKG=$(pwd | cut -d '/' -f 5)
-else
-    echo "当前不是Termux或MT管理器环境，退出!"
-    exit 1
-fi
+pwd | grep termux || echo "当前不是Termux环境，退出!" && exit 1
 
 # 安装 Shizuku
-if [ -f "$DIR/rish" -a -f "$DIR/rish_shizuku.dex" ]; then
+if [ -f "$DIR/rish" ] && [ -f "$DIR/rish_shizuku.dex" ]; then
     echo "Shizuku已安装,是否重新安装？(y/n): "
-    read input
+    read -r input
     if [ "$input" = "y" ]; then
         # 强删 rish dex
-        rm -rf $DIR/rish*
+        rm -rf "$DIR/rish*"
         install && usage
     fi
 else
